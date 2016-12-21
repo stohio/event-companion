7756f8aa0e656acc67708a63034b862a655e48be
agates4 <agate97@gmail.com>
2016-12-20 20:18:56 -0500

    Initial Commit

---
+++ Hackron/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hackron
+//
+//  Created by Aron Gates on 12/20/16.
+//  Copyright © 2016 Aron Gates. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
